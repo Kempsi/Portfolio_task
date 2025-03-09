e7fc688cff64a716ac39d64307140f43e6ffc97e
Niko Kemppainen <2306099@edu.karelia.fi>
2025-03-09 18:45:50 +0200

    Tehtäviin lisätty osio scrolleri

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+ * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+ */
 
+// Smooth scroll tapahtumankuuntelija .scroll-btn napeille
+document.querySelectorAll(".scroll-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const id = this.getAttribute("data-target");
+    const element = document.querySelector(id);
 
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop - 20,
+        behavior: "smooth",
+      });
+    }
+  });
+});
